3d5adc40a928548856f7ffa2c665a18595c3582a
anasofiaelizondo <a01723045@tec.mx>
2022-10-26 17:36:35 -0500

    Initial commit

---
+++ dia 7/practicas(dia 8)/uno-variables.js
@@ -0,0 +1,13 @@
+const myName = "Ana Sofia"
+let myAge = 16
+let x = "frutas"
+let y = "verduras"
+var likesMusic = true
+
+console.log(typeof myName)
+console.log(typeof myAge)
+console.log(typeof x)
+console.log(typeof y)
+console.log(typeof likesMusic)
+
+
